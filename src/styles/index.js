bc36d066460ab10a308cbc9ff3318d8a6ad996c5
Anderson <andersonbarreto2001@gmail.com>
2023-07-11 18:08:04 +0200

    adding more propertys

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,8 +1,20 @@
 export const theme = {
   fonts: {
-    general: 'var(--ff-font-general)'
+    body: 'var(--ff-primary-body)',
+    heading: 'var(--ff-primary-headline)',
+    size: '18px',
+    colorBase: '#ffffff'
   },
   fontWeight: {
     bold: 'bold'
+  },
+  sizes: {
+    mobileS: '320px',
+    mobileM: '375px',
+    mobileL: '425px',
+    tablet: '768px',
+    laptop: '1024px',
+    laptopL: '1440px',
+    desktop: '2560px'
   }
 }
