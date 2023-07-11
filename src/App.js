a807a50bfcf0481c89fe56c4258d003ed993b194
Anderson <andersonbarreto2001@gmail.com>
2023-07-11 12:40:38 +0200

    mod imports

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import NavBar from './components/Navbar'
 
-function App() {
+const App = () => {
+  const optionsMenu = ['Sobre Mi', 'Poryectos', 'CV']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <NavBar options={optionsMenu} />
+    </>
+
+  )
 }
 
-export default App;
+export default App
