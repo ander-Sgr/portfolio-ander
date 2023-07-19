feceeaa29c5a35e5ce4278995a302ee53d5ff875
Anderson <andersonbarreto2001@gmail.com>
2023-07-20 00:07:18 +0200

    toggle switch done

--- src/components/ToggleSwitch/index.jsx
+++ src/components/ToggleSwitch/index.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import sunIcon from './icons/sunIcon.svg'
 import moonIcon from './icons/moonIcon.svg'
-import { Label, SunIcon, MoonIcon, BallSwitch } from './styles'
+import {
+  Label,
+  SunIcon,
+  MoonIcon,
+  BallSwitch,
+  ContainerSwitch,
+  CheckBox
+} from './styles'
+import { ThemeContext } from '../../contexts/ThemeContext'
 
 const ToggleSwitch = () => {
+  const [isChecked, setIsChecked] = useState(false)
+  const { changeTheme } = useContext(ThemeContext)
+
+  const handleOnchangeCheckBox = () => {
+    setIsChecked(!isChecked)
+    changeTheme()
+  }
   return (
-    <Label>
-      <SunIcon src={sunIcon} alt='sunIcon' />
-      <MoonIcon src={moonIcon} alt='moonIcon' />
-      <BallSwitch />
-    </Label>
+    <ContainerSwitch>
+      <CheckBox
+        id='toggle'
+        type='checkbox'
+        checked={isChecked}
+        onChange={handleOnchangeCheckBox}
+      />
+      <Label htmlFor='toggle'>
+        <MoonIcon src={moonIcon} alt='moonIcon' />
+        <SunIcon src={sunIcon} alt='sunIcon' />
+        <BallSwitch isChecked={isChecked} />
+      </Label>
+    </ContainerSwitch>
+
   )
 }
 
--- src/components/ToggleSwitch/styles.js
+++ src/components/ToggleSwitch/styles.js
@@ -1,37 +1,46 @@
 import styled from '@emotion/styled/macro'
 
+export const ContainerSwitch = styled.div``
+
 export const Label = styled.label`
-   background-color: #111;
-   width: 50px;
-   height: 22px;
-   border-radius: 50px;
-   position: relative;
-   padding: 5px;
-   cursor: pointer;
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
+  background-color: #ffff;
+  width: 50px;
+  height: 22px;
+  border-radius: 50px;
+  position: relative;
+  padding: 5px;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `
 
 export const BallSwitch = styled.span`
-  ${Label} & {
-    background-color: #fff;
-    width: 26px;
-    height: 26px;
+   ${Label} > & {
+    background-color: #28ca59;
+    width: 22px;
+    height: 22px;
     position: absolute;
-    left: 2px;
-    top: 2px;
+    left: 4px;
     border-radius: 50%;
     transition: transform 0.2s linear;
   }
 `
 
-const IconBase = styled.img`
-    width: 14px;
+export const CheckBox = styled.input`
+  position: absolute;
+  opacity: 0;
+  &:checked ~ ${Label} > ${BallSwitch}  {
+    transform: translateX(${({ isChecked }) => (
+      isChecked ? '5px' : '32px'
+    )});
+  }
 `
 
-export const SunIcon = styled(IconBase)`
-  
+const IconBase = styled.img`
+  width: 13px;
 `
-export const MoonIcon = styled(IconBase)`
-`
+
+export const SunIcon = styled(IconBase)``
+
+export const MoonIcon = styled(IconBase)``
