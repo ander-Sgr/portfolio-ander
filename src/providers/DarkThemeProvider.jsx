f73032cac58870cd98ee93b6d024c1fd15026d69
Anderson <andersonbarreto2001@gmail.com>
2023-07-22 14:41:28 +0200

    feat: save the theme in the localSotorage

--- src/providers/DarkThemeProvider.jsx
+++ src/providers/DarkThemeProvider.jsx
@@ -5,13 +5,23 @@ import { theme } from '../styles'
 export const DarkThemeProvider = ({ children }) => {
   const [currentTheme, setCurrentTheme] = useState(theme.dark)
 
+  useEffect(() => {
+    const storedTheme = localStorage.getItem('theme')
+    if (storedTheme) {
+      setCurrentTheme(JSON.parse(storedTheme))
+    }
+  }, [])
+
   const changeTheme = () => {
-    setCurrentTheme(currentTheme === theme.dark ? theme.ligth : theme.dark)
+    // setCurrentTheme(currentTheme === theme.dark ? theme.ligth : theme.dark)
+    setCurrentTheme((prevTheme) =>
+      prevTheme === theme.dark ? theme.ligth : theme.dark
+    )
   }
   // TO DO save the theme in the locaLStorage..
   useEffect(() => {
-
-  }, [])
+    localStorage.setItem('theme', JSON.stringify(currentTheme))
+  }, [currentTheme])
 
   return (
     <ThemeContext.Provider value={{ currentTheme, changeTheme }}>
