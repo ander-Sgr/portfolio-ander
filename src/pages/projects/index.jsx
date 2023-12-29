b9260b284adf7ea0e5e71ceb04f5ca4b342617f3
Anderson <andersonbarreto2001@gmail.com>
2023-12-29 13:55:32 +0100

    move component PorjectCard

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -3,9 +3,26 @@ import CardProject from '../../components/CardProject'
 import { Container } from './styles'
 
 const Projects = () => {
+  const stack = ['js', 'html']
+
   return (
     <Container>
-      <CardProject />
+      <CardProject
+        imgProject='minesweeper'
+        title='MINESWEEPER'
+        iconTech={stack}
+        descrip='Este proyecto fue hecho para probar mis habilidades en JS HTML Y CSS . Fue realiado durente mi Internship en Travelport'
+      />
+      <CardProject
+        imgProject='minesweeper'
+        title='MINESWEEPER'
+        iconTech={stack}
+      />
+      <CardProject
+        imgProject='minesweeper'
+        title='MINESWEEPER'
+        iconTech={stack}
+      />
     </Container>
   )
 }
