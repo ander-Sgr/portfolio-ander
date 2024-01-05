0001bd84aa4dba4bf08c018c62ed92ae13e00f61
Anderson <77341608+ander-Sgr@users.noreply.github.com>
2024-01-05 13:32:53 +0000

    fix arrays

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -3,7 +3,8 @@ import CardProject from '../../components/CardProject'
 import { Container } from './styles'
 
 const Projects = () => {
-  const stack = ['js', 'html']
+  const stack = ['js', 'html', 'css', 'playwright']
+  const stackLaravle = ['']
 
   return (
     <Container>
@@ -11,12 +12,17 @@ const Projects = () => {
         imgProject='minesweeper'
         title='MINESWEEPER'
         iconTech={stack}
-        descrip='Este proyecto fue hecho para probar mis habilidades en JS HTML Y CSS . Fue realiado durente mi Internship en Travelport'
+        descrip='Este es un proyecto hecho durante mi Internship. Fue realizado para poner a prueba
+        mis habilidades en JS, HTML/CSS y en realizar test end-to-end con el uso
+        de la biblioteca playwrigth para automatizar los tests'
+        urlCode='https://github.com/ander-Sgr/minesweeper'
+        urlProject='https://ander-sgr.github.io/minesweeper/src/'
       />
       <CardProject
         imgProject='minesweeper'
         title='MINESWEEPER'
         iconTech={stack}
+        descrip='Este proyecto fue hecho para probar mis habilidades en JS HTML Y CSS . Fue realiado durente mi Internship en Travelport'
       />
       <CardProject
         imgProject='minesweeper'
