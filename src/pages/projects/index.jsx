573bfb05747bbc1199eb7c033d0bb7f7ace4396e
Anderson <andersonbarreto2001@gmail.com>
2024-01-08 15:20:47 +0100

    update descrip

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -3,31 +3,31 @@ import CardProject from '../../components/CardProject'
 import { Container } from './styles'
 
 const Projects = () => {
-  const stack = ['js', 'html', 'css', 'playwright']
-  const stackLaravle = ['']
+  const stackMinesweeper = ['js', 'html', 'css', 'playwright']
+  const stackPostApp = ['']
 
   return (
     <Container>
       <CardProject
         imgProject='minesweeper'
         title='MINESWEEPER'
-        iconTech={stack}
+        iconTech={stackMinesweeper}
         descrip='Este es un proyecto hecho durante mi Internship. Fue realizado para poner a prueba
         mis habilidades en JS, HTML/CSS y en realizar test end-to-end con el uso
-        de la biblioteca playwrigth para automatizar los tests'
+        de la biblioteca playwright para automatizar los tests'
         urlCode='https://github.com/ander-Sgr/minesweeper'
         urlProject='https://ander-sgr.github.io/minesweeper/src/'
       />
       <CardProject
-        imgProject='minesweeper'
-        title='MINESWEEPER'
-        iconTech={stack}
-        descrip='Este proyecto fue hecho para probar mis habilidades en JS HTML Y CSS . Fue realiado durente mi Internship en Travelport'
+        imgProject='postApp'
+        title='POST-APP'
+        iconTech={stackPostApp}
+        descrip='Con la creación de esta aplicación'
       />
       <CardProject
         imgProject='minesweeper'
-        title='MINESWEEPER'
-        iconTech={stack}
+        title='LOL-TRACKER-GG'
+
       />
     </Container>
   )
