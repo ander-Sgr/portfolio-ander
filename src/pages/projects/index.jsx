e00dc2e33fffd040f5ca4268345dd56872069205
Anderson <andersonbarreto2001@gmail.com>
2024-01-09 13:35:26 +0100

    adding URL posts-app deploy

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -28,6 +28,7 @@ const Projects = () => {
         así como la creación de factorias, migraciciones y poblacion de la base de datos con datos mock. Todo esto hecho con
         Laravel, HTML, CSS y como base de datos HeidiSQL'
         urlCode='https://github.com/ander-Sgr/posts-app'
+        urlProject='https://posts-app-production-f496.up.railway.app/'
       />
 
     </Container>
