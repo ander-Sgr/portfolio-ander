b9e3c276553816117be248aacad20be5f931bf8e
Anderson <77341608+ander-Sgr@users.noreply.github.com>
2024-08-16 12:37:18 +0000

    feat: wip timeline work experience

---
+++ src/components/TimeLine/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const TimeLineWork = ({ nameCompany, titleJob, location, dateStart, descriptionJob }) => {
+  return (
+    <></>
+  )
+}
+
+export default TimeLineWork
