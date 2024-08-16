0c3a8d79d621b4b04faab25c8b09385e3b25065d
Anderson <77341608+ander-Sgr@users.noreply.github.com>
2024-08-16 11:28:26 +0000

    apply responsive

--- src/pages/aboutMe/index.jsx
+++ src/pages/aboutMe/index.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
-import { ContainerIntro, IntroText, LineSeparator, SubTitle } from './styles'
+import {
+  ContainerIntro,
+  IntroText,
+  LineSeparator,
+  SubTitle,
+  ContainerContact,
+  Contact,
+  ContactImage,
+  DescriptionContainer
+} from './styles'
+import linkedinsvg from './svgs/linkedin.svg'
+import githubsvg from './svgs/github.svg'
 
 const AboutMe = () => {
   return (
@@ -7,6 +18,18 @@ const AboutMe = () => {
       <IntroText>Hey, Soy <br /> Anderson
         <SubTitle>Web Developer</SubTitle>
       </IntroText>
+      <DescriptionContainer>
+        Apasionado desarrollador web con expriencia práctica,
+        Actualmente me estoy redirigiendo mi carrera a la cultura Devops.
+      </DescriptionContainer>
+      <ContainerContact>
+        <Contact href='https://www.linkedin.com/in/anderson-estrella-barreto/' target='_blank'>
+          <ContactImage src={linkedinsvg} alt='linkdin logo' /> Linkedin
+        </Contact>
+        <Contact href='https://github.com/ander-Sgr/' target='_blank'>
+          <ContactImage src={githubsvg} alt='github logo' /> Github
+        </Contact>
+      </ContainerContact>
       <LineSeparator />
     </ContainerIntro>
   )
--- src/pages/aboutMe/styles.js
+++ src/pages/aboutMe/styles.js
@@ -1,21 +1,78 @@
 import styled from '@emotion/styled'
 
 export const ContainerIntro = styled.div`
-    margin-left: 125px;
+   margin: 0 auto;
+   padding-top: 68px;
+   width: 70%;
+   @media (max-width: 920px) {
+    width: 70%;
+    padding-top: 18px;
+
+   }
+   @media (max-width: 650px) {
+    width: 90%;
+    padding-top: 18px;
+
+   }
 `
 
 export const IntroText = styled.h1`
     font-family: ${(props) => props.theme.fonts.lexendFont};
     color: ${(props) => props.theme.foreground};
-    font-size: 100px;
+    font-size: 72px;
+    margin-bottom: 14px;
+    @media (max-width: 550px) {
+        font-size: 62px;
+        margin-top: 0px;
+    }
 `
 export const SubTitle = styled(IntroText)`
-    font-size: 42px;
-    color: #FF2626;
-    line-height: 0.5;
+    font-size: 35px;
+    color: #D64550;
+    margin: 0;
+    @media (max-width: 550px) {
+        font-size: 32px;
+    }
 `
 export const LineSeparator = styled.div`
-    width: 641px;
-    padding: 1px;
+    width: 100%;
+    padding-top: 1px;
+    margin-top: 13px;
     background-color: #FFFFFF;
+`
+export const ContainerContact = styled.div`
+    display: grid;
+    padding-bottom: 15px;
+    grid-template-columns: repeat(auto-fill, 100px);
+    gap: 18px;
+    align-items: center;
+    text-align: center;
+`
+export const Contact = styled.a`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+    padding: 10px;
+    gap: 8px;
+    background-color: #162435;
+    cursor: pointer;
+    text-decoration: none;
+    color: #FFFF;
+
+`
+export const ContactImage = styled.img`
+    width: 20%;
+`
+export const DescriptionContainer = styled.div`
+    font-size: 20px;
+    padding: 10px 0px 25px 0px;
+    width: 60%;
+
+    @media (max-width: 920px) {
+        width: 70%;
+    }
+   @media (max-width: 650px) {
+        width: 90%;
+   }
 `
